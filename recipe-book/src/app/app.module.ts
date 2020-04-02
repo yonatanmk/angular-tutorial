e648bd3577ad5895dd01ab20e8eb033fd128768e
yonatanmk <yonatanmk@gmail.com>
2020-04-02 18:03:43 -0400

    recipe book prepare components

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -7,7 +7,8 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
     RecipeListComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
