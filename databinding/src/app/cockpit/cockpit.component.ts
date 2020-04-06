6d86f97b9cc4f33973d5ab802f1cb6bcb668d67e
yonatanmk <yonatanmk@gmail.com>
2020-04-06 11:56:00 -0400

    local template references

--- databinding/src/app/cockpit/cockpit.component.ts
+++ databinding/src/app/cockpit/cockpit.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
-  @Output() blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
-  newServerName = '';
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  // newServerName = '';
   newServerContent = '';
 
   constructor() { }
@@ -16,16 +16,16 @@ export class CockpitComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({ 
-      serverName: this.newServerName, 
+      serverName: nameInput.value, 
       serverContent: this.newServerContent 
     })
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({ 
-      serverName: this.newServerName, 
+      serverName: nameInput.value, 
       serverContent: this.newServerContent 
     })
   }
--- databinding/src/app/server-element/server-element.component.ts
+++ databinding/src/app/server-element/server-element.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  // encapsulation: ViewEncapsulation.None
+  // encapsulation: ViewEncapsulation.Native
 })
 export class ServerElementComponent implements OnInit {
   @Input('serverElement') element: { type: string, name: string, content: string };
