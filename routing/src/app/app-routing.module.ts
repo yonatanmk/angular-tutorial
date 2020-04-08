ca045d50450f7dc7dcb7047496738f84c7ed8c53
yonatanmk <yonatanmk@gmail.com>
2020-04-08 15:59:50 -0400

    login authenticaion route guards

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -9,15 +9,23 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+import { AuthGuard } from './auth-guard.service';
+
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent, children: [
     { path: ':id/:name', component: UserComponent },
   ] },
-  { path: 'servers', component: ServersComponent, children: [
-    { path: ':id', component: ServerComponent },
-    { path: ':id/edit', component: EditServerComponent },
-  ] },
+  { 
+    path: 'servers', 
+    // canActivate: [AuthGuard], 
+    canActivateChild: [AuthGuard], 
+    component: ServersComponent, 
+    children: [
+      { path: ':id', component: ServerComponent },
+      { path: ':id/edit', component: EditServerComponent },
+    ],
+  },
   { path: 'not-found', component: PageNotFoundComponent },
   { path: '**', redirectTo: '/not-found' }, // must be last item in array
 ];
