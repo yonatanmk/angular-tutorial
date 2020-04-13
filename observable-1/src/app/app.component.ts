3f89aee0be5551a345790fc87e269e83e094c632
yonatanmk <yonatanmk@gmail.com>
2020-04-13 15:40:42 -0400

    bugfix

--- observable-1/src/app/app.component.ts
+++ observable-1/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UserService } from './user.service';
 import { Subscription } from 'rxjs';
 
